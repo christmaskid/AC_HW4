bc3459bad65ca4ae29cbf790bca7d59082e95639
root <root@LAPTOP-LG7CJA98>
2024-12-09 02:07:57 +0800

    Expression setup

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -15,11 +15,18 @@ using namespace llvm;
 
 namespace {
 
-void print_value(Value* v) {
-	if (v->hasName())
-		errs() << v->getName();
+void print_value(Value v) {
+	if (v.hasName())
+		errs() << v.getName();
 	else
-		errs() << *v;
+		errs() << v;
+}
+void print_bitvector(BitVector bv) {
+	int n = bv.size();
+	for(int i=0;i<n;i++) {
+		errs() << bv[i];
+	}
+	errs() << "\n";
 }
 
 struct Expression {
@@ -44,7 +51,6 @@ struct Expression {
 		return false;
 	}
 	bool operator==(const Expression &x) const {
-		errs() << "compare" << *I <<" and "<<*(x.I)<<"\n";
 		if (x.dest != dest)
 			return false;
 		if (x.opcode != opcode) 
@@ -57,7 +63,6 @@ struct Expression {
 			if (x.operands[i] != operands[i])
 				return false;
 		}
-		errs()<<"same.\n";
 		return true;
 	}
 };
@@ -74,26 +79,25 @@ Expression InstrToExpr(Instruction* I) {
 	expr.dest = I; // the destination valu is the instruction itself
 	expr.opcode = I->getOpcode();
 
-	errs() << expr.dest << " = " << " " << I->getOpcodeName() << " ";
+	// errs() << expr.dest << " = " << " " << I->getOpcodeName() << " ";
 
 	// https://stackoverflow.com/questions/44946645/traversal-of-llvm-operands
 	for(auto iter = I->op_begin(); iter != I->op_end(); ++iter) {
 		Value* op = *iter;
 		expr.operands.push_back(op);
-		errs() << "operand:";
-		print_value(op);
-		errs() << " ";
+		// errs() << "operand:";
+		// print_value(*op);
+		// errs() << " ";
 	}
-	errs() << "\n";
+	// errs() << "\n";
 
-	for(auto iter = I->user_begin(); iter != I->user_end(); ++iter) {
-		Value* op = *iter;
-		expr.operands.push_back(op);
-		errs() << "user:";
-		print_value(op);
-		errs() << " ";
-	}
-	errs() << "\n";
+	// for(auto iter = I->user_begin(); iter != I->user_end(); ++iter) {
+		// Value* user = *iter;
+		// errs() << "user:";
+		// print_value(*user);
+		// errs() << " ";
+	// }
+	// errs() << "\n";
 
 	return expr;
 }
@@ -123,6 +127,75 @@ struct BasicBlockInfo {
 
 };
 
+template <typename Iterator>
+void scanBlockAndMark(BitVector &target, int set_value, \
+	Iterator b_begin, Iterator b_end,\
+	std::map<Expression, unsigned int> exprmap) {
+
+	int n = exprmap.size();
+	BitVector defined;
+	defined.resize(n);
+	defined.clear();
+
+	for(auto it = b_begin; it != b_end; it++) {
+		Instruction &I = *it;
+		errs() << I << "\n";
+
+		Expression I_expr = InstrToExpr(&I);
+		if (exprmap.find(I_expr) != exprmap.end()) {
+			defined[exprmap[I_expr]] = 1;
+		}
+		errs() << "defined:"; print_bitvector(defined);
+
+		for(auto &op: I.operands()) {
+			Expression op_expr = InstrToExpr(dyn_cast<Instruction>(op));
+			auto it = exprmap.find(op_expr);
+			if (it == exprmap.end())
+				continue;
+			unsigned bit = it->second;
+			if (defined[bit])
+				target[bit] = set_value;
+		}
+	}
+}
+void setExpr(BasicBlockInfo* bbinfo, std::map<Expression, unsigned int> exprmap) {
+	int n = exprmap.size();
+
+	// ExprKill: = Definition
+	bbinfo->ExprKill.clear();
+	for(int i=0;i<n;i++) {
+		bbinfo->ExprKill[i] = bbinfo->Exprs[i];
+	}
+
+	// DEExpr:
+	// the expression is evaluated AFTER (re)definition within the same block, 
+	// and its operands are not redefined afterwards
+
+	/* 	For each instruction:
+			record that this expr is defined (eg. %1 = %2 + %3 --> defined[%1]=1)
+			for each operand : instruction
+				if operand has been defined: mark as YES // evaluated
+
+		For each instruction in reverse order:
+			record that this expr is defined (eg. %1 = %2 + %3 --> defined[%1]=1)
+			for each operand : instruction
+				if operand has been defined: mark as NO
+	*/
+	bbinfo->DEExpr.resize(n);
+	bbinfo->DEExpr.clear();
+	scanBlockAndMark(bbinfo->DEExpr, 1, bbinfo->B->begin(), bbinfo->B->end(), exprmap);
+	scanBlockAndMark(bbinfo->DEExpr, 0, bbinfo->B->rbegin(), bbinfo->B->rend(), exprmap);
+	errs() << "DEExpr: ";
+	print_bitvector(bbinfo->DEExpr);
+
+	// UEExpr: reverse
+	bbinfo->UEExpr.resize(n);
+	bbinfo->UEExpr.clear();
+	scanBlockAndMark(bbinfo->UEExpr, 1, bbinfo->B->rbegin(), bbinfo->B->rend(), exprmap);
+	scanBlockAndMark(bbinfo->UEExpr, 0, bbinfo->B->begin(), bbinfo->B->end(), exprmap);
+	errs() << "UEExpr: ";
+	print_bitvector(bbinfo->UEExpr);
+}
 
 
 struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
@@ -155,6 +228,7 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
     	// 	bbinfo.n_succ++;
     	// 	bbinfo.succs.push_back(succ);
     	// }
+    	int i = 0;
     	for (auto &I : B) {
     		Expression expr = InstrToExpr(&I);
     		bbinfo.exprs.push_back(expr);
