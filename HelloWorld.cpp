a736c643dc445c02c367484b1552420e798e7579
chengyutong <chengyt2001@gmail.com>
2024-12-23 03:13:33 +0800

    It finally works!!!

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -16,6 +16,12 @@
 #include "llvm/ADT/DepthFirstIterator.h"
 #include "llvm/ADT/PostOrderIterator.h"
 #include <map> // DenseMap is hard to use...
+#include <string>
+
+// Ref.: https://stackoverflow.com/questions/60514223/how-to-insert-a-basic-block-between-two-block-in-llvm
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+
+#include "llvm/Transforms/Utils/Cloning.h"
 
 typedef std::pair<llvm::BasicBlock*, llvm::BasicBlock*> BBpair;
 
@@ -200,8 +206,8 @@ template <typename Iterator>
 void scanBlockAndMark(BitVector &target, int set_value, \
 	Iterator b_begin, Iterator b_end, std::map<Expression, unsigned int> exprmap) {
 
-	// A O(n^2) method.
-
+	// Get definitions and uses
+	// A O(n^2) method. Maximum size of operands in an instruction is constant (<=3).
 	for(auto it = b_begin; it != b_end; ++it) {
 		Instruction &I = *it;
 		// errs() << "I " << I << "\n";
@@ -226,6 +232,7 @@ void scanBlockAndMark(BitVector &target, int set_value, \
 			}
 		}
 	}
+
 }
 
 
@@ -300,31 +307,12 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 	std::map<BasicBlock*, BasicBlockInfo> blockmap;
 	SmallVector<BBpair, 8> edges;
 	std::map<BBpair, EdgeInfo> edgemap;
-	SmallVector<BasicBlock*, 16> RRPOlist;
 
-	void buildRRPOiter(BasicBlock* B, SmallVector<BasicBlock*, 16> &worklist, \
-		std::map <BasicBlock*, bool> &visited) {
-		if (succ_empty(B)) return;
-		for(BasicBlock* succ : successors(B)) {
-			if (!visited[succ]) {
-				visited[succ] = true;
-				buildRRPOiter(succ, worklist, visited);
-			}
-		}
-		worklist.push_back(B);
-	}
-
-	void buildTraversalInfo(Function* F, SmallVector<BasicBlock*, 16> &worklist) {
-		std::map <BasicBlock*, bool> visited;
-		visited.clear();
-		for(auto &B : *F) {
-			visited[&B] = false;
-		}
-
-		BasicBlock* entryBlock = &(F->getEntryBlock());
-		visited[entryBlock] = true;
-		buildRRPOiter(entryBlock, worklist, visited);
-	}
+	// In the forward problem, we can always do reverse postorder traversal on the function's
+	// CFG because there is always only one entry block.
+	// However, we may not be able to do the reverse postorder traversal on the reversed CFG
+	// because there may be more than one exit blocks.
+	// Then, the worklist algorithm cannot end in one rounds -> worklist still needed.
 
 	void init(Function *F) {
 		exprmap_n = 0;
@@ -332,9 +320,6 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 		inv_exprmap.clear();
 		// blocks.clear();
 		blockmap.clear();
-		RRPOlist.clear();
-
-		buildTraversalInfo(F, RRPOlist);
 	}
 
 	void buildExprKill(BasicBlockInfo* bbinfo) {
@@ -382,31 +367,12 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 		// Initialization
 		for(auto B : ReversePostOrderTraversal<BasicBlock*>(entryBlock)) {
 			blockmap[B].AvailOut.reset();
-			blockmap[B].AvailOut.flip();
 			blockmap[B].AvailIn.reset();
+			blockmap[B].AvailIn.flip();			
 		}
 		blockmap[entryBlock].AvailIn.reset();
 
 		for(auto B : ReversePostOrderTraversal<BasicBlock*>(entryBlock)) {
-
-			B->printAsOperand(errs(), false);
-			
-			if (!pred_empty(B)) {
-				// errs() << *B << "\n";
-				// errs() << "AvailIn "; print_bitvector(blockmap[B].AvailIn); errs()<<"\n";
-
-				int flag = 0;
-				for(BasicBlock *pred : predecessors(B)) {
-					if (flag)
-						blockmap[B].AvailIn &= blockmap[pred].AvailOut;
-					else
-						blockmap[B].AvailIn |= blockmap[pred].AvailOut;
-					flag = 1;
-
-					// errs()<<"& "; pred->printAsOperand(errs(), false); print_bitvector(blockmap[pred].AvailOut); errs()<<"\n";
-					// errs()<<"= ";print_bitvector(blockmap[B].AvailIn); errs()<<"\n";
-				}
-			}
 
 			BitVector negExprkill = blockmap[B].ExprKill;
 			negExprkill.flip();
@@ -415,6 +381,12 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 			blockmap[B].AvailOut |= blockmap[B].AvailIn;
 			blockmap[B].AvailOut &= negExprkill;
 			blockmap[B].AvailOut |= blockmap[B].DEExpr;
+
+			if (!succ_empty(B)) {
+				for(BasicBlock *succ : successors(B)) {
+					blockmap[succ].AvailIn &= blockmap[B].AvailOut;
+				}
+			}
 
 		}
 		errs() << "\n";
@@ -435,36 +407,60 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 		BasicBlock* entryBlock = &(F->getEntryBlock());
 
 		// Initialization
-		for(auto B : RRPOlist) {
-			blockmap[B].AntOut.reset();
+		for(auto &BB : *F) {
+			BasicBlock* B = &BB;
 			blockmap[B].AntIn.reset();
-			blockmap[B].AntIn.flip(); // initialize with all expressions
-		}
-
-		for(auto B : RRPOlist) {
-			B->printAsOperand(errs(), false);
-			errs() << " ";
-
 			blockmap[B].AntOut.reset();
-			if (!succ_empty(B)) {
-				int flag = 0;
-				for(BasicBlock *succ: successors(B)) {
-					if (flag)
-						blockmap[B].AntOut &= blockmap[succ].AntIn;
-					else
-						blockmap[B].AntOut |= blockmap[succ].AntIn;
-					flag = 1;
+			if (!succ_empty(B))
+				blockmap[B].AntOut.flip(); // initialize with all expressions
+		}
+		// Worklist algorithm
+		bool changed = true;
+		while (changed) {
+			changed = false;
+
+			for(auto B : post_order(F)) {
+				B->printAsOperand(errs(), false);
+				errs() << " ";
+
+				BitVector oldVector(blockmap[B].AntIn);
+
+				BitVector negExprkill = blockmap[B].ExprKill;
+				negExprkill.flip();
+				// blockmap[B].AntIn = blockmap[B].UEExpr | (blockmap[B].AntOut & negExprkill);
+				blockmap[B].AntIn.reset();
+				blockmap[B].AntIn |= blockmap[B].AntOut;
+				blockmap[B].AntIn &= negExprkill;
+				blockmap[B].AntIn |= blockmap[B].UEExpr;
+
+				for(int i=0; !changed && i<exprmap_n; i++) {
+					if (oldVector[i] != blockmap[B].AntIn[i]) {
+						changed = true;
+						break;
+					}
+				}
+
+				if (!pred_empty(B)) {
+					for(BasicBlock *pred : predecessors(B)) {
+						BitVector oldVector(blockmap[pred].AntOut);
+						blockmap[pred].AntOut &= blockmap[B].AntIn;
+
+						B->printAsOperand(errs(), false);
+						errs() << ".AntIn = ";
+						print_bitvector(blockmap[B].AntIn);
+						errs() << " & -> ";
+						pred->printAsOperand(errs(), false);
+						errs() << ".AntOut = ";
+						print_bitvector(blockmap[pred].AntOut);
+
+						for(int i=0; !changed && i<exprmap_n; i++) {
+							if (oldVector[i] != blockmap[pred].AntOut[i]) {
+								changed = true;
+							}
+						}
+					}
 				}
 			}
-
-			BitVector negExprkill = blockmap[B].ExprKill;
-			negExprkill.flip();
-			// blockmap[B].AntIn = blockmap[B].UEExpr | (blockmap[B].AntOut & negExprkill);
-			blockmap[B].AntIn.reset();
-			blockmap[B].AntIn |= blockmap[B].AntOut;
-			blockmap[B].AntIn &= negExprkill;
-			blockmap[B].AntIn |= blockmap[B].UEExpr;
-
 		}
 		errs() << "\n";
 	}
@@ -482,42 +478,28 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 			negAntOut_i.flip();
 			
 			// Earliest(i,j) = AntIn(j) & !(AvailOut(i)) & (ExprKill(i) + !AntOut(i))
-			// errs() << "buildEarliest "<<edgeinfo<<"\n";
-			// printEdgeInfo(edgeinfo);
 			edgeinfo->Earliest.reset();
-			// errs() << "Build earliest "; print_bitvector(edgeinfo->Earliest); errs()<<"\n"; 
 			edgeinfo->Earliest |= blockmap[i].ExprKill;
-			// errs() << "= ExprKill "; print_bitvector(blockmap[i].ExprKill); errs()<<"\n";
 			edgeinfo->Earliest |= negAntOut_i;
-			// errs() << "+ !AntOut(i) "; print_bitvector(negAntOut_i); errs()<<"\n";
 			edgeinfo->Earliest &= blockmap[j].AntIn;
-			// errs() << "&  AntIn(j)"; print_bitvector(blockmap[j].AntIn); errs()<<"\n";
 			edgeinfo->Earliest &= negAvailOut_i;
-			// errs() << "&  !AvailOut(i)"; print_bitvector(negAvailOut_i); errs()<<"\n";
-			// errs() << "= "; print_bitvector(edgeinfo->Earliest); errs()<<"\n"; 
 		}
 	}
 
 	// Forward flow
-	void buildLater(Function &F) {
-		BasicBlock* entryBlock = &(F.getEntryBlock());
+	void buildLater(Function* F) {
+		// Initialization
+		for (auto &BB : *F) {
+			BasicBlock* B = &BB;
+			blockmap[B].LaterIn.reset();
+			blockmap[B].LaterIn.flip();
+		}
+		BasicBlock* entryBlock = &(F->getEntryBlock());
+		blockmap[entryBlock].LaterIn.reset();
+
 		for(auto B : ReversePostOrderTraversal<BasicBlock*>(entryBlock)) {
 			// errs()<<"buildLater "<<B->getName()<<" "<<pred_size(B)<<" "<<pred_empty(B)<<"\n";
 
-			blockmap[B].LaterIn.reset();
-			if (!pred_empty(B)) {
-				blockmap[B].LaterIn.flip();
-
-				for(BasicBlock* pred : predecessors(B)) {
-					BBpair pair = std::make_pair(pred, B);
-					EdgeInfo* edgeinfo = &(edgemap[pair]);
-					BasicBlockInfo* bbinfo_i = &(blockmap[pred]);
-					BasicBlockInfo* bbinfo_j = &(blockmap[B]);
-
-					bbinfo_j->LaterIn &= edgeinfo->Later;
-
-				}
-			}
 			if (!succ_empty(B)) {
 				for(BasicBlock* succ : successors(B)) {
 					BBpair pair = std::make_pair(B, succ);
@@ -535,6 +517,8 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 					edgeinfo->Later |= edgeinfo->Earliest;
 					// errs() << "| Earliest(i,j) "; print_bitvector(edgeinfo->Earliest); errs()<<"\n";
 					// errs() << "= "; print_bitvector(edgeinfo->Later); errs()<<"\n"; 
+
+					bbinfo_j->LaterIn &= edgeinfo->Later;
 				}
 			}
 		}
@@ -554,7 +538,6 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 			BasicBlock* i = edgeinfo->start;
 			blockmap[i].Delete.reset();
 			if (i != &(F.getEntryBlock())) {
-				blockmap[i].Delete.reset();
 				blockmap[i].Delete |= blockmap[i].UEExpr;
 				BitVector negLaterIn_i = blockmap[i].LaterIn;
 				negLaterIn_i.flip();
@@ -636,7 +619,7 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 
 	    // Edges
 	    buildEarliest();
-	    buildLater(F);
+	    buildLater(&F);
 
 	    // Code motion
 	    buildInsertDelete(F);
@@ -650,18 +633,64 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 	}
 
 	void rewriteInsert(Function* F) {
+		int cnt = 0;
 		for(auto &edge : edges) {
 			EdgeInfo* edgeinfo = &(edgemap[edge]);
+			if (edgeinfo->Insert.empty())
+				continue;
+
+			cnt++;
 			BasicBlock* i = edgeinfo->start;
 			BasicBlock* j = edgeinfo->end;
 
-			IRBuilder<> builder_i(i);
+			int choice = -1;
+			IRBuilder<> *builder = nullptr;
 
-			for(int idx = 0; idx < exprmap_n; idx++) {
+			if (!i->getSingleSuccessor()) {
+				choice = 1;
+				builder = new IRBuilder<>(i->getContext());
+			} else if (!j->getSinglePredecessor()) {
+				choice = 2;
+				builder = new IRBuilder<>(j->getContext());
+			} else {
+				choice = 3;
+				// Don't put them into the function yet!!!
+				// Ref.: https://stackoverflow.com/questions/61181446/how-to-build-a-empty-basicblock-and-insert-a-instruction-in-the-front
+				LLVMContext &context = i->getContext();
+				builder = new IRBuilder<>(i->getContext());
+				edgeinfo->InsertBlock = BasicBlock::Create(context, "newBlock" + std::to_string(cnt));
+
+				errs()<<"Insert new block between ";
+				i->printAsOperand(errs(), false);
+				errs()<<" and ";
+				j->printAsOperand(errs(), false);
+				errs()<<":\n";
+			}
+
+			for(int idx = exprmap_n-1; idx >= 0; idx--) {
 				if (!edgeinfo->Insert[idx]) continue;
 				Expression* expr = &(inv_exprmap[idx]);
-				Instruction* cloned_instr = expr->I;//->clone();
+				Instruction* cloned_instr = expr->I->clone();
+				errs()<<*cloned_instr<<"\n";
 
+				switch(choice) {
+					case 1:
+					// If i has only one successor, we insert the expressions at the end of block i.
+						builder->SetInsertPoint(i->getTerminator());
+						builder->Insert(cloned_instr);
+						break;
+					case 2:
+					// else If j has only one predecessor, we insert the expressions at the entry of block j.
+						builder->SetInsertPoint(j->getFirstInsertionPt());
+						builder->Insert(cloned_instr);
+						break;
+					case 3:
+					// else, we split the the edge (i, j) by creating a new block to insert the expressions.
+						builder->SetInsertPoint(edgeinfo->InsertBlock->getFirstInsertionPt());
+						builder->Insert(cloned_instr);
+				}
+
+				// Ref.: https://stackoverflow.com/questions/30319747/when-and-where-did-the-llvm-ir-instruction-set-its-parent-basicblock
 
 				// Ref. Cornell course, LLVM for grad: addtomul example
     			// Everywhere the old instruction was used as an operand,
@@ -670,27 +699,8 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
     				User* user = U.getUser(); // A User is anything with operands
     				user->setOperand(U.getOperandNo(), cloned_instr);
     			}
-
-				errs() << "Insert " << *cloned_instr << " from " << *(expr->I) << "\n";
-
-				if (succ_size(i)==1) {
-					errs() << "insert the expression at the end of block i\n";
-					cloned_instr->insertInto(i, i->end());
-				}
-				else if(pred_size(j)==1) {
-					errs() << "insert the expression at the entry of block j\n";
-					cloned_instr->insertBefore(i->begin());
-				}
-				else {
-					errs() << "split the edge (i,j) by creating a new block in between\n";
-					LLVMContext &Context = F->getContext();
-					BasicBlock* newBlock = BasicBlock::Create(Context, "new_block", F);
-
-					i->getTerminator()->replaceUsesOfWith(j, newBlock);
-					BranchInst::Create(j, newBlock);
-					cloned_instr->insertBefore(newBlock->begin());
-				}
 			}
+			delete builder;
 		}
 	}
 	void rewriteDelete(Function* F) {
@@ -704,10 +714,39 @@ struct HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
 			}
 		}
 	}
+	void cleanUpNewBlocks(Function* F) {
+		// Put all newly created blocks into the function
+		// Ref.: https://stackoverflow.com/questions/61181446/how-to-build-a-empty-basicblock-and-insert-a-instruction-in-the-front
+		for(auto &edge : edges) {
+			EdgeInfo* edgeinfo = &(edgemap[edge]);
+			BasicBlock* i = edgeinfo->start;
+			BasicBlock* j = edgeinfo->end;
+
+			if (!(edgeinfo->InsertBlock))
+				continue;
+			// Set function parent and upstream node
+			BasicBlock* newBlock = edgeinfo->InsertBlock;
+			newBlock->insertInto(F, i);
+
+			// Set descendant
+			Instruction *terminator = i->getTerminator();
+			int n = terminator->getNumSuccessors();
+			for(unsigned edge_num=0; edge_num<n; ++edge_num) {
+				if (terminator->getSuccessor(edge_num)==j) {
+					terminator->setSuccessor(edge_num, newBlock);
+					break;
+				}
+			}
+			// Add terminator instruction (BranchInst) into the end ot fhe new block
+			IRBuilder<> builder(newBlock);
+			builder.CreateBr(j);
+		}
+	}
 
 	void codeMotion(Function* F) {
 		rewriteInsert(F);
 		rewriteDelete(F);
+		cleanUpNewBlocks(F);
 	}
 
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
